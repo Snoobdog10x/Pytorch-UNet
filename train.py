9c4e4cb42c7e648f2cb605d8205c69f0339ea9cd
Nguyen Duy Thanh <duythanh1565@gmail.com>
2023-04-20 07:00:51 +0700

    deactive amp

--- train.py
+++ train.py
@@ -197,7 +197,7 @@ def get_args():
     parser.add_argument('--scale', '-s', type=float, default=0.5, help='Downscaling factor of the images')
     parser.add_argument('--validation', '-v', dest='val', type=float, default=10.0,
                         help='Percent of the data that is used as validation (0-100)')
-    parser.add_argument('--amp', action='store_true', default=True, help='Use mixed precision')
+    parser.add_argument('--amp', action='store_true', default=False, help='Use mixed precision')
     parser.add_argument('--model_type', '-mt', type=str, default="SMALL",
                         help='choose model type: NORMAL or LITE or SMALL')
     parser.add_argument('--save_epoch_plot', '-sep', type=bool, default=True,
